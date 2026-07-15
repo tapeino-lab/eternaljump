f8a231d619d9b23c732ec15b7553697600c7dc1b
tapeino-lab <44920784+tapeino-lab@users.noreply.github.com>
2026-07-15 22:40:24 +0900

    バグ修正

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './', plugins: [react(), tailwindcss()],
+    base: process.env.VITE_BASE_PATH || './', plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
